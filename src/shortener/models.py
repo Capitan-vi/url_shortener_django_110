6fd580fb7dabae1b23fca238fa0c1b1e1f45b4e9
Valentin Emelyanov <valentin.html@gmail.com>
2021-02-13 20:28:43 +0300

    intro: Model manager , refresh shortcode method (lesson 16)

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,19 +1,38 @@
-import random
-import string
 from django.db import models
 
+from .utils import code_generator, create_shortcode
+
 # Create your models here.
-def code_generator(size=6, chars= string.ascii_lowercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+class KirrURLManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs_main = super(KirrURLManager, self).all(*args, **kwargs)
+        qs = qs_main.filter(active=True)
+        return qs
+
+    def refresh_shortcodes(self):
+        qs = KirrURL.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            print(q.shortcode)
+            q.save()
+            new_codes += 1
+        return "New codes made: {i}".format(i=new_codes)
 
 class KirrURL(models.Model):
     """docstring for KirrURL"""
     url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=15, unique=True)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    objects = KirrURLManager()
+    some_random = KirrURLManager()
 
     def save(self, *args, **kwargs):
-        print('something')
-        self.shortcode = code_generator()
+        if self.shortcode is None or self.shortcode == "":
+            self.shortcode = create_shortcode(self)
         super(KirrURL, self).save(*args, **kwargs)
 
     def __str__(self):
