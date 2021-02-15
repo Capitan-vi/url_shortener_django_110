e2ea691f70c37b80c71a20b96e8e44843b207887
Valentin Emelyanov <valentin.html@gmail.com>
2021-02-15 21:26:34 +0300

    Url keyword arguments (lesson 20)

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
-def kirr_redirect_view(request, *args, **kwargs):   #function based view
-    return HttpResponse("hello")
+def kirr_redirect_view(request, shortcode=None, *args, **kwargs):   #function based view
+    return HttpResponse("HELLO {sc}".format(sc=shortcode))
 
 class KirrCBView(View):   #class based view
-    def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello again!")
+    def get(self, request, shortcode=None, *args, **kwargs):
+        return HttpResponse("HELLO AGAIN {sc}".format(sc=shortcode))
 
