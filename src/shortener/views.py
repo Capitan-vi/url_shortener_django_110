f51f51479d50182abd8f089d4286b32637d66836
Valentin Emelyanov <valentin.html@gmail.com>
2021-02-18 18:43:04 +0300

    Query DB with the shortcode (lesson 21)

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+
+from .models import KirrURL
 
 # Create your views here.
 def kirr_redirect_view(request, shortcode=None, *args, **kwargs):   #function based view
-    return HttpResponse("HELLO {sc}".format(sc=shortcode))
+    obj = get_object_or_404(KirrURL, shortcode=shortcode)
+    return HttpResponse("HELLO {sc}".format(sc=obj.url))
 
 class KirrCBView(View):   #class based view
     def get(self, request, shortcode=None, *args, **kwargs):
+        obj = get_object_or_404(KirrURL, shortcode=shortcode)
         return HttpResponse("HELLO AGAIN {sc}".format(sc=shortcode))
 
+    def post(self, request, *args, **kwargs):
+        return HttpResponse()
