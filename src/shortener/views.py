5d0688efe9730dae463a9a6fe29b327d0e53ec48
Valentin Emelyanov <valentin.html@gmail.com>
2021-02-19 23:47:52 +0300

    HttpResponse redirect and URL ordering (lesson 22)

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,18 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from .models import KirrURL
 
 # Create your views here.
+def test_view(request):
+    return HttpResponse("some test response stuff")
+
 def kirr_redirect_view(request, shortcode=None, *args, **kwargs):   #function based view
     obj = get_object_or_404(KirrURL, shortcode=shortcode)
-    return HttpResponse("HELLO {sc}".format(sc=obj.url))
+    #do something
+    return HttpResponseRedirect(obj.url)
 
 class KirrCBView(View):   #class based view
     def get(self, request, shortcode=None, *args, **kwargs):
         obj = get_object_or_404(KirrURL, shortcode=shortcode)
-        return HttpResponse("HELLO AGAIN {sc}".format(sc=shortcode))
+        return HttpResponseRedirect(obj.url)
 
     def post(self, request, *args, **kwargs):
         return HttpResponse()
