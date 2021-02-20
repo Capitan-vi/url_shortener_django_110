9e26d19a0e0b8c22f373ba1886cf7585bb251df5
Valentin Emelyanov <valentin.html@gmail.com>
2021-02-20 23:19:37 +0300

    Using django hosts for subdomain handling (lesson 24)

--- src/kirr/settings.py
+++ src/kirr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nv^2cs21rcad79a%m=_ghs0p%-iy=5$3*))*6iyhphu@e9c&zk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.tirr.com', 'tirr.co', 'www.kirr.co', 'kirr.co']
 
 
 # Application definition
@@ -38,11 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #third party apps
+    'django_hosts',
+
     #custom apps
     'shortener',
 ]
 
 MIDDLEWARE = [
+    'django_hosts.middleware.HostsRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,9 +54,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_hosts.middleware.HostsResponseMiddleware'
 ]
 
 ROOT_URLCONF = 'kirr.urls'
+ROOT_HOSTCONF = 'kirr.hosts'
+DEFAULT_HOST = 'www'
+DEFAULT_REDIRECT_URL = 'http://www.tirr.com:8000'
 
 TEMPLATES = [
     {
