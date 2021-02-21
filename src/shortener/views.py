dd03714d64ad5027118967e2cc5d02b3d66cdd3a
Valentin Emelyanov <valentin.html@gmail.com>
2021-02-21 16:01:20 +0300

    Basic html form and post action set up (lesson 26)

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -9,6 +9,12 @@ class HomeView(View):
     def get(self, request, *args, **kwargs):
         return render(request, "shortener/home.html", {})
 
+    def post(self, request, *args, **kwargs):
+        print(request.POST)
+        print(request.POST["url"])
+        print(request.POST.get("url"))
+        return render(request, "shortener/home.html", {})
+
 class KirrCBView(View):   #class based view
     def get(self, request, shortcode=None, *args, **kwargs):
         obj = get_object_or_404(KirrURL, shortcode=shortcode)
